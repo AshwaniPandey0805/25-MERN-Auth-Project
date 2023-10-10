74ae33f0bca376fcc7db84eab0a30155efbed71c
Ashwani Pandey <ashwanipandey679@gmail.com>
2023-10-11 00:09:16 +0530

    final commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,21 +8,21 @@ export default function Header() {
     
     return (
         <div className='bg-slate-200' >
-            <div className="flex justify-between items-center max-w-6xl mx-auto p-3"  >
+            <div className="flex justify-evenly items-center max-w-6xl mx-auto p-4 "  >
                 <Link to="/">
                     <h1 className='font-bold' >Auth app</h1>    
                 </Link>
                 
                 <ul className='flex gap-4'>
                     <Link to="/" >
-                    <li>Home</li>
+                    <li className="font-bold" >Home</li>
                     </Link>
                     <Link to="/about" >
-                        <li>About</li>
+                        <li className="font-bold" >About</li>
                     </Link>
                     <Link to="/profile" >
                         { currentUser ? (
-                            <li><img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover" /></li>
+                            <li><img src={currentUser.profilePicture} alt="profile" className="h-8 w-8 rounded-full object-cover " /></li>
                         ) : (
                             <li>Sign -in</li>    
                         )}
